22a1c16d6406624c8c045855a8c8369aba4ab388
areysid <sidharthmalpani@gmail.com>
2026-02-20 03:06:11 +0530

    applied theme & styling to the app

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -71,9 +71,11 @@ export function LoginForm() {
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
                                     <Button variant="outline" className="w-full" type="button" disabled={isPending}>
+                                        <Image alt="Git Hub" src="/logos/github.svg" width={20} height={20} />
                                         Continue with GitHub
                                     </Button>
                                     <Button variant="outline" className="w-full" type="button" disabled={isPending}>
+                                         <Image alt="Google" src="/logos/google.svg" width={20} height={20} />
                                         Continue with Google
                                     </Button>
                                 </div>
