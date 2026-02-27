78839b27068b6161df87d0929e883017bbd27480
areysid <sidharthmalpani@gmail.com>
2026-02-28 01:29:12 +0530

    added workflows-pagination using NUQS

---
+++ src/hooks/use-entity-search.tsx
@@ -0,0 +1,56 @@
+import { PAGINATION } from "@/config/constants";
+import { useEffect, useState } from "react";
+
+
+interface UseEntitySearchProps<T extends {
+    search: string;
+    page: number
+}> {
+    params: T;
+    setParams: (params: T) => void;
+    debounceMs?: number;
+}
+
+export function UseEntitySearch<T extends {
+    search: string;
+    page: number;
+}>({
+    params, 
+    setParams,
+    debounceMs = 500
+}: UseEntitySearchProps<T>) {
+    const [localSearch, setLoacalSearch] = useState(params.search);
+
+    useEffect(() => {
+        if (localSearch === "" && params.search !== "") {
+            setParams({
+                ...params,
+                search: "",
+                page: PAGINATION.DEFAULT_PAGE
+            });
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            if (localSearch != params.search) {
+                setParams({
+                    ...params,
+                    search: localSearch,
+                    page: PAGINATION.DEFAULT_PAGE
+                })
+            }
+        }, debounceMs);
+
+        return () => clearTimeout(timer);
+
+    }, [localSearch, params, setParams, debounceMs]);
+
+    useEffect(() => {
+        setLoacalSearch(params.search)
+    }, [params.search]);
+
+    return {
+        searchValue: localSearch,
+        onSearchChange: setLoacalSearch,
+    };
+};
