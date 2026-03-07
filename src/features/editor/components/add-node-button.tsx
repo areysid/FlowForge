046bea9a502fa86e73a3affe1b9b73af1d5987ac
areysid <sidharthmalpani@gmail.com>
2026-03-07 13:35:09 +0530

    added nodes as well as node-selector component

--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { NodeSelector } from "@/components/node-selector";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 export const AddNodeButton = memo(() => {
+
+    const [selectorOpen, setSelectorOpen] = useState(false)
+
     return (
+        <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
         <Button
         onClick={() => {}}
         size="icon"
@@ -14,6 +19,7 @@ export const AddNodeButton = memo(() => {
         >
             <PlusIcon />
         </Button>
+        </NodeSelector>
     )
 });
 
