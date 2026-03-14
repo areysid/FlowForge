0602f5ea5990fe09f903e6fe978dd54102768664
areysid <sidharthmalpani@gmail.com>
2026-03-14 14:06:15 +0530

    made changes for the demo purposes for mid term presentation

--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -8,7 +8,7 @@ export const ManualTriggerNode = memo((props: NodeProps) => {
 
     const [ dialogOpen, setDialogOpen ] = useState(false);
 
-    const nodeStatus = "initial"
+    const nodeStatus = "loading"
 
     const handleOpenSettings = () => setDialogOpen(true);
 
