a1f215e3570e63081659529e1252c7d9afb866eb
Dhruv <dhruvjoshi1069@gmail.com>
2026-05-06 13:27:46 +0530

    28: encrypting credentials

--- src/features/credentials/server/routers.ts
+++ src/features/credentials/server/routers.ts
@@ -4,6 +4,7 @@ import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/i
 import z, { positive, string } from "zod";
 import { PAGINATION } from "@/config/constants";
 import { CredentialType } from "@/generated/prisma/enums";
+import { encrypt } from "@/lib/encryption";
 
 export const credentialsRouter = createTRPCRouter({
     create: premiumProcedure
@@ -22,7 +23,7 @@ export const credentialsRouter = createTRPCRouter({
                 name,
                 userId: ctx.auth.user.id,
                 type,
-                value, //TODO: Consider encrypting in production
+                value: encrypt(value),
             },
         });
     }),
@@ -56,7 +57,7 @@ export const credentialsRouter = createTRPCRouter({
                 data: {
                     name,
                     type,
-                    value, //TODO: Consider encrypting
+                    value: encrypt(value),
                 }
             });
         }),
